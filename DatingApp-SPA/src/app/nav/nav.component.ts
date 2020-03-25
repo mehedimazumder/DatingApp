e7e385b2d00b098936a819db4d5501423d945e94
mehedi <mehedirumc12cuet14@gmail.com>
2020-03-25 07:17:49 +0600

    decodeToken of angular jwt

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
   constructor(
-    private authService: AuthService,
+    public authService: AuthService,
     private alertify: AlertifyService
   ) {}
 
