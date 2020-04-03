fe5472a70ad7d66a6c70625aae771cb28b633bb5
mehedi <mehedirumc12cuet14@gmail.com>
2020-04-03 18:57:27 +0600

    Implementing pagination

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using DatingApp.API.Helpers;
 using DatingApp.API.Model;
 
 namespace DatingApp.API.Data
@@ -10,7 +11,7 @@ namespace DatingApp.API.Data
         void Add<T>(T entitty) where T : class;
         void Delete<T>(T entitty) where T : class;
         Task<bool> SaveAll();
-        Task<IEnumerable<User>> GetUsers();
+        Task<PagedList<User>> GetUsers(UserParams userParams);
         Task<User> GetUser(int id);
         Task<Photo> GetPhoto(int id);
         Task<Photo> GetMainPhotoForUser(int userId);
--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -4,10 +4,10 @@ namespace DatingApp.API.Helpers
     {
         private const int MaxPageSize = 50;
         public int PageNumber { get; set; } = 1;
-        private int pageSize;
+        private int pageSize = 10;
         public int PageSize
         {
-            get { return pageSize; }
+            get { return pageSize = 10; }
             set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
         }
 
