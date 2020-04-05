1d1765c99b2370ced447d827c59c68aa8c8e9b8e
mehedi <mehedirumc12cuet14@gmail.com>
2020-04-05 23:51:55 +0600

    Like functionality to SPA

--- DatingApp-SPA/src/app/members/member-card/member-card.component.ts
+++ DatingApp-SPA/src/app/members/member-card/member-card.component.ts
@@ -1,15 +1,35 @@
+import { AlertifyService } from './../../_services/alertify.service';
+import { UserService } from 'src/app/_services/user.service';
 import { User } from './../../_models/user';
 import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
+  styleUrls: ['./member-card.component.css'],
 })
 export class MemberCardComponent implements OnInit {
   @Input() user: User;
 
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
+
+  sendLike(recipientId: number) {
+    this.userService
+      .sendLike(this.authService.decodedToken.nameid, recipientId)
+      .subscribe(
+        (data) => {
+          this.alertify.success('You have liked ' + this.user.knownAs);
+        },
+        (error) => {
+          this.alertify.error(error);
+        }
+      );
+  }
 }
