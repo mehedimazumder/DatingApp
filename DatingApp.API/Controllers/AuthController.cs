8f929af0b567a43a0273fb288c0c689626c98ab6
mehedi <mehedirumc12cuet14@gmail.com>
2020-04-29 23:41:00 +0600

    Role added to JWT token

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -73,13 +74,20 @@ namespace DatingApp.API.Controllers
 
         }
 
-        private string GenerateToken(User user)
+        private async Task<string> GenerateToken(User user)
         {
-            var claims = new[]
+            var claims = new List<Claim>
            {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                 new Claim(ClaimTypes.Name, user.UserName)
             };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
 
             var key = new SymmetricSecurityKey(Encoding.UTF8
                 .GetBytes(_config.GetSection("AppSetting:Token").Value));
