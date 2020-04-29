8a3b0ca839ca1ce9d1dec01b2cd350de9eb88c9e
mehedi <mehedirumc12cuet14@gmail.com>
2020-04-30 00:50:44 +0600

    getting all user with roles

--- DatingApp.API/Controllers/AdminController.cs
+++ DatingApp.API/Controllers/AdminController.cs
@@ -1,5 +1,9 @@
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -7,18 +11,38 @@ namespace DatingApp.API.Controllers
     [Route("api/[controller]")]
     public class AdminController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public AdminController(DataContext context)
+        {
+            _context = context;
+        }
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("usersWithRoles")]
-        public IActionResult GetUsersWithRoles()
+        public async Task<IActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admin can see this");
+            var userList = await _context.Users
+                .OrderBy(x => x.UserName)
+                .Select(user => new
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Roles = (from userRole in user.UserRoles
+                             join role in _context.Roles
+                             on userRole.RoleId
+                             equals role.Id
+                             select role.Name).ToList()
+                }).ToListAsync();
+
+            return Ok(userList);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photosForModeration")]
         public IActionResult GetPhotosForModeration()
         {
-            return Ok("Only admin can see this");
+            return Ok("Only admin or moderator can see this");
         }
     }
 }
